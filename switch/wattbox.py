660a06f42891a22ab9963c709366f9ac22008ce6
Greg Badros <badros@gmail.com>
2018-05-10 19:47:00 -0700

    Added, first version; needs pywattbox

---
+++ switch/wattbox.py
@@ -0,0 +1,57 @@
+"""
+Support for WattBox outlet switches
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/light.ketra/
+"""
+import logging
+
+from homeassistant.components.switch import SwitchDevice
+from .. import wattbox
+#(WattBoxDevice, WATTBOX_DEVICES, WATTBOX_CONTROLLER)
+
+_LOGGER = logging.getLogger(__name__)
+
+DEPENDENCIES = ['wattbox']
+
+# pylint: disable=unused-argument
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Set up the wattbox power strip switches."""
+    devs = []
+    for (area_name, device) in hass.data[wattbox.WATTBOX_DEVICES]['switch']:
+        dev = WattBoxSwitch(area_name, device, hass.data[wattbox.WATTBOX_CONTROLLER])
+        devs.append(dev)
+
+    add_devices(devs, True)
+    _LOGGER.warning("Added " + devs)
+    return True
+
+
+class WattBoxSwitch(wattbox.WattBoxDevice, SwitchDevice):
+    """Representation of a WattBox outlet, just on and off."""
+
+    def __init__(self, area_name, wattbox_switch, controller):
+        """Initialize the light."""
+        wattbox.WattBoxDevice.__init__(self, area_name, wattbox_switch, controller)
+        self._prev = None
+
+    def turn_on(self, **kwargs):
+        """Turn the switch on."""
+        self._wattbox_switch._set_state(True)
+        self.schedule_update_ha_state()
+        
+
+    def turn_off(self, **kwargs):
+        """Turn the light off."""
+        self._wattbox_switch._set_state(False)
+        self.schedule_update_ha_state()
+
+    @property
+    def is_on(self):
+        """Return true if device is on."""
+        return self._wattbox_switch._on
+
+    def update(self):
+        """Call when forcing a refresh of the device."""
+        if self._wattbox_switch.update():
+            self.schedule_update_has_state()
---
+++ wattbox.py
@@ -0,0 +1,88 @@
+"""
+Component for interacting with a WattBox ip-controlled power strip.
+
+For more details about this component, please refer to the documentation at
+https://home-assistant.io/components/wattbox/
+"""
+import asyncio
+import logging
+
+import voluptuous as vol
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD
+from homeassistant.const import CONF_PAYLOAD_OFF, CONF_PAYLOAD_ON
+from homeassistant.helpers import discovery
+from homeassistant.helpers.entity import Entity
+
+REQUIREMENTS = ['pywattbox==0.0.1']
+
+DOMAIN = 'wattbox'
+
+_LOGGER = logging.getLogger(__name__)
+
+WATTBOX_CONTROLLER = 'wattbox_controller'
+WATTBOX_DEVICES = 'wattbox_devices'
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_HOST): cv.string,
+        vol.Required(CONF_USERNAME): cv.string,
+        vol.Required(CONF_PASSWORD): cv.string,
+        vol.Optional("area"): cv.string, #FIXME
+        vol.Optional("noop_set_state"): cv.boolean, #FIXME
+    })
+}, extra=vol.ALLOW_EXTRA)
+
+
+def setup(hass, base_config):
+    """Set up the WattBox component."""
+    from pywattbox import WattBox
+
+    hass.data[WATTBOX_CONTROLLER] = None
+    hass.data[WATTBOX_DEVICES] = {'switch': []}
+    
+    config = base_config.get(DOMAIN)
+    area = config.get('area', 'wattbox')
+    hass.data[WATTBOX_CONTROLLER] = WattBox(
+        config[CONF_HOST], config[CONF_USERNAME],
+        config[CONF_PASSWORD], area, config.get("noop_set_state"))
+
+    hass.data[WATTBOX_CONTROLLER].load_xml()
+    _LOGGER.info("Loaded config from Wattbox at %s", config[CONF_HOST])
+
+    # Sort our devices into types
+    for switch in hass.data[WATTBOX_CONTROLLER].switches:
+        _LOGGER.info("adding switch %s", switch)
+        hass.data[WATTBOX_DEVICES]['switch'].append((area, switch))
+
+    discovery.load_platform(hass, 'switch', DOMAIN, None, base_config)
+    return True
+
+
+class WattBoxDevice(Entity):
+    """Representation of a Ketra device entity."""
+
+    def __init__(self, area_name, wattbox_switch, controller):
+        """Initialize the device."""
+        self._wattbox_switch = wattbox_switch
+        self._controller = controller
+        self._area_name = area_name
+
+    @asyncio.coroutine
+    def async_added_to_hass(self):
+        """Register callbacks."""
+
+    def _update_callback(self, _switch):
+        """Run when invoked by pywattbox when the device state changes."""
+        self.schedule_update_ha_state()
+
+    @property
+    def name(self):
+        """Return the name of the device."""
+        return "{} {}".format(self._area_name, self._wattbox_switch.name)
+
+    @property
+    def should_poll(self):
+        """No polling needed."""
+        return True
